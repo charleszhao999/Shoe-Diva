e2de2366b38a7186e81c232add9b6e7e3eb97c84
Jan29th <charleszhao999@163.com>
2022-12-25 20:18:14 +0800

    添加项目文件。

---
+++ Shoe Diva/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Shoe_Diva
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(596, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shoe Diva";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Location = new System.Drawing.Point(952, -12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(414, 471);
+            this.panel1.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel2.Controls.Add(this.label9);
+            this.splitContainer1.Panel2.Controls.Add(this.label8);
+            this.splitContainer1.Panel2.Controls.Add(this.label7);
+            this.splitContainer1.Panel2.Controls.Add(this.label6);
+            this.splitContainer1.Panel2.Controls.Add(this.label5);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Size = new System.Drawing.Size(414, 471);
+            this.splitContainer1.SplitterDistance = 138;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.Location = new System.Drawing.Point(116, 251);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 32;
+            this.dataGridView1.Size = new System.Drawing.Size(126, 188);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(116, 194);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 24);
+            this.label9.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(116, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 24);
+            this.label8.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(116, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 24);
+            this.label7.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 24);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "订购客户";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 24);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "库存量";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "价　格";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "出产地";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(86, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "商品详情";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Shoe_Diva.Properties.Resources.loafers;
+            this.pictureBox1.Location = new System.Drawing.Point(63, 132);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(324, 132);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(585, 132);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(846, 132);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "购买";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "购买";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(585, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 34);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "购买";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(846, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 34);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "购买";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1363, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private SplitContainer splitContainer1;
+        private DataGridView dataGridView1;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+    }
+}
---
+++ Shoe Diva/Form1.cs
@@ -0,0 +1,132 @@
+using System.Data;
+
+namespace Shoe_Diva
+{
+    public partial class Form1 : Form
+    {
+        int flag = 0;
+        bool[] available = new bool[] { true, true, true, true };
+        Image[] imgs;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
+            Image a = pictureBox1.Image;
+            Image b = pictureBox2.Image;
+            Image c = pictureBox3.Image;
+            Image d = pictureBox4.Image;
+            imgs = new Image[] { a, b, c, d };
+            initItems();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            flag = 0;
+            refreshInfo();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Purchase purchase = new Purchase(1);
+            purchase.Show();
+            initItems();
+            refreshInfo();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Purchase purchase = new Purchase(2);
+            purchase.Show();
+            initItems();
+            refreshInfo();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Purchase purchase = new Purchase(3);
+            purchase.Show();
+            initItems();
+            refreshInfo();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Purchase purchase = new Purchase(4);
+            purchase.ShowDialog();
+            initItems();
+            refreshInfo();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            flag = 1;
+            refreshInfo();
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            flag = 2;
+            refreshInfo();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            flag = 3;
+            refreshInfo();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            flag = 4;
+            refreshInfo();
+        }
+        private void refreshInfo()
+        {
+            if (flag != 0 && available[flag-1])
+            {
+                DataTable shoe = DBHelper.GetDataSet("select * from Shoes where SId=" + flag).Tables[0];
+                label2.Text = shoe.Rows[0]["model"].ToString();
+                label7.Text = shoe.Rows[0]["origin"].ToString();
+                label8.Text = shoe.Rows[0]["prize"].ToString();
+                label9.Text = shoe.Rows[0]["inventory"].ToString();
+                dataGridView1.DataSource = DBHelper.GetDataSet("select customer from Orders where SId=" + flag).Tables[0];
+            }
+            else
+            {
+                label2.Text = "…Ã∆∑œÍ«È";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+                dataGridView1.DataSource = null;
+            }
+        }
+        private void initItems()
+        {
+            PictureBox[] pics = new PictureBox[] { pictureBox1, pictureBox2, pictureBox3, pictureBox4 };
+            Button[] buttons = new Button[] { button1, button2, button3, button4 };
+            for (int i = 0; i < 4; i++)
+            {
+                DataSet ds = DBHelper.GetDataSet("select * from Shoes where SId="+(i+1));
+                if (ds.Tables[0].Rows.Count == 0)
+                {
+                    pics[i].Image = Properties.Resources.sold_out;
+                    buttons[i].Enabled = false;
+                    available[i] = false;
+                }
+                else
+                {
+                    pics[i].Image = imgs[i];
+                    buttons[i].Enabled = true;
+                    available[i] = true;
+                }
+            }
+        }
+    }
+}
